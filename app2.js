b5248a4330c8796063fb701803baa33c4ab90c31
phlames <kelechiikeolu@gmail.com>
2024-11-28 02:00:45 +0100

    Feat: Started hero section

---
+++ app2.js
@@ -0,0 +1,15 @@
+const toggleMenu = document.getElementById("toggleMenu");
+const hideMenu = document.getElementById("hideMenu");
+const sidebar = document.getElementById("sidebar");
+
+toggleMenu.addEventListener("click", () => {
+  sidebar.classList.toggle("show");
+  hideMenu.classList.toggle("showClose");
+  toggleMenu.classList.add("hideMenu");
+});
+
+hideMenu.addEventListener("click", () => {
+  sidebar.classList.remove("show");
+  hideMenu.classList.remove("showClose");
+  toggleMenu.classList.remove("hideMenu");
+});
